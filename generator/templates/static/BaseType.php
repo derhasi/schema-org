3d335cd5b296d6df37ded30ed288077c23f5d74d
Johannes Haseitl <johannes@undpaul.de>
2017-02-07 06:28:34 +0100

    Add some sparse docblocks before converting to php5

--- generator/templates/static/BaseType.php
+++ generator/templates/static/BaseType.php
@@ -10,16 +10,28 @@ abstract class BaseType implements Type
     /** @var array */
     protected $properties = [];
 
+    /**
+     * @return string
+     */
     public function getContext(): string
     {
         return 'http://schema.org';
     }
 
+    /**
+     * @return string
+     */
     public function getType(): string
     {
         return (new ReflectionClass($this))->getShortName();
     }
 
+    /**
+     * @param string $property
+     * @param $value
+     *
+     * @return $this
+     */
     public function setProperty(string $property, $value)
     {
         $this->properties[$property] = $value;
@@ -27,7 +39,13 @@ abstract class BaseType implements Type
         return $this;
     }
 
-    public function if($condition, $callback)
+    /**
+     * @param bool $condition
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function if($condition, callable $callback)
     {
         if ($condition) {
             $callback($this);
@@ -36,16 +54,28 @@ abstract class BaseType implements Type
         return $this;
     }
 
+    /**
+     * @param string $property
+     * @param null $default
+     *
+     * @return mixed|null
+     */
     public function getProperty(string $property, $default = null)
     {
         return $this->properties[$property] ?? $default;
     }
 
+    /**
+     * @return array
+     */
     public function getProperties(): array
     {
         return $this->properties;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function toArray(): array
     {
         $properties = $this->serializeProperty($this->getProperties());
@@ -56,6 +86,12 @@ abstract class BaseType implements Type
         ] + $properties;
     }
 
+    /**
+     * @param $property
+     *
+     * @return array
+     * @throws \Spatie\SchemaOrg\Exceptions\InvalidProperty
+     */
     protected function serializeProperty($property)
     {
         if (is_array($property)) {
@@ -74,11 +110,17 @@ abstract class BaseType implements Type
         return $property;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function toScript(): string
     {
         return '<script type="application/ld+json">'.json_encode($this->toArray()).'</script>';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function __toString(): string
     {
         return $this->toScript();
